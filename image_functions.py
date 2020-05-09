d8700b5f852ea586d4aea9c120d4084cca7bebf0
lstubbolo <lstubbol@udel.edu>
2020-05-09 14:15:14 -0400

    takeSource f'n now uses getFullPath for both default and positional args changed order of f'ns in image_functions to better match order of buttons

--- image_functions.py
+++ image_functions.py
@@ -24,7 +24,8 @@ except ModuleNotFoundError:
 #   Functions used in OCR sampling Runs
 
 #   takes the source picture
-def takeSource(srcPath=util.getFullPath('source.jpg'), debug=False):
+def takeSource(srcPath='source.jpg', debug=False):
+    srcPath = util.getFullPath(srcPath)
     if debug:
         print(f"Capturing Source Image, saving to \n\t'{srcPath}'")
 
@@ -75,90 +76,6 @@ def cropSource(src_path='source.jpg', debug=False):
 
 
 #   Functions used by Cropping Setup
-
-
-#   shows a video ###currently broken af
-def showVid():
-    with PiCamera() as camera:
-        # camera.resolution = (640, 480)
-        camera.resolution = (800, 480)
-
-        camera.framerate = 32
-        # rawCapture = PiRGBArray(camera, size=(640, 480))
-        rawCapture = PiRGBArray(camera, size=(800, 480))
-
-        windowName = "Live Feed"
-        cv2.namedWindow(windowName, cv2.WINDOW_NORMAL)
-
-        # terminate = False
-        param = []
-
-        #   set window size, move window
-        cv2.resizeWindow(windowName, SCREEN_DIMS['width'], SCREEN_DIMS['height'])
-
-        #   load in the crop objs
-        cropObjs = coordList()
-
-        for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-            # grab the raw NumPy array representing the image, then initialize the timestamp
-            # and occupied/unoccupied text
-            image = frame.array
-
-            #   draw the rectangles on the image
-            for obj in cropObjs.myList[1:]:
-                cv2.rectangle(image, obj.topL, obj.botR, (0, 255, 0), 3)
-
-            # show the frame
-            cv2.imshow(windowName, image)
-
-            cv2.setMouseCallback(windowName, closeEvent, param)
-            cv2.waitKey(1) & 0xFF
-
-            # clear the stream in preparation for the next frame
-            rawCapture.truncate(0)
-            # if the `q` key was pressed, break from the loop
-
-            if param:
-                break
-
-    cv2.destroyWindow(windowName)
-    print("Outside of loop\n\n")
-
-
-#   shows the source image with the bounding areas for cropping
-def showImage(imgPath=util.getFullPath('source.jpg')):
-    if not os.path.exists(imgPath):
-        print("No Source Image, Fool! Run takeSource!")
-        return
-
-    else:
-        #   load image from path, create a named window
-        image = cv2.imread(imgPath)
-        windowName = "Source Image"
-        cv2.namedWindow(windowName, cv2.WINDOW_NORMAL)
-
-        #   set window size, move window
-        cv2.resizeWindow(windowName, SCREEN_DIMS['width'], SCREEN_DIMS['height'])
-        # cv2.moveWindow(windowName, 0, -50)
-
-        #   add mouse listener function
-        cv2.setMouseCallback(windowName, closeEvent)
-
-        cropObjs = coordList()
-
-        #   add rectangles to the window
-        for obj in cropObjs.myList[1:]:
-            cv2.rectangle(image, obj.topL, obj.botR, (0, 255, 0), 3)
-
-        cv2.imshow(windowName, image)
-        cv2.waitKey(0)
-
-
-#   Use this event listener if you want to close after one click only
-def closeEvent(event, x, y, flags, param):
-    if event == cv2.EVENT_LBUTTONUP:
-        print("Closing...")
-        param.append(True)
 
 
 #   adds an entry to cropFile.json
@@ -252,3 +169,87 @@ def addCropHandler(event, x, y, flags, param):
 
             print("closing window ")
             cv2.destroyAllWindows()
+
+
+#   shows a video ###currently broken af
+def showVid():
+    with PiCamera() as camera:
+        # camera.resolution = (640, 480)
+        camera.resolution = (800, 480)
+
+        camera.framerate = 32
+        # rawCapture = PiRGBArray(camera, size=(640, 480))
+        rawCapture = PiRGBArray(camera, size=(800, 480))
+
+        windowName = "Live Feed"
+        cv2.namedWindow(windowName, cv2.WINDOW_NORMAL)
+
+        # terminate = False
+        param = []
+
+        #   set window size, move window
+        cv2.resizeWindow(windowName, SCREEN_DIMS['width'], SCREEN_DIMS['height'])
+
+        #   load in the crop objs
+        cropObjs = coordList()
+
+        for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+            # grab the raw NumPy array representing the image, then initialize the timestamp
+            # and occupied/unoccupied text
+            image = frame.array
+
+            #   draw the rectangles on the image
+            for obj in cropObjs.myList[1:]:
+                cv2.rectangle(image, obj.topL, obj.botR, (0, 255, 0), 3)
+
+            # show the frame
+            cv2.imshow(windowName, image)
+
+            cv2.setMouseCallback(windowName, closeEvent, param)
+            cv2.waitKey(1) & 0xFF
+
+            # clear the stream in preparation for the next frame
+            rawCapture.truncate(0)
+            # if the `q` key was pressed, break from the loop
+
+            if param:
+                break
+
+    cv2.destroyWindow(windowName)
+    print("Outside of loop\n\n")
+
+
+#   shows the source image with the bounding areas for cropping
+def showImage(imgPath=util.getFullPath('source.jpg')):
+    if not os.path.exists(imgPath):
+        print("No Source Image, Fool! Run takeSource!")
+        return
+
+    else:
+        #   load image from path, create a named window
+        image = cv2.imread(imgPath)
+        windowName = "Source Image"
+        cv2.namedWindow(windowName, cv2.WINDOW_NORMAL)
+
+        #   set window size, move window
+        cv2.resizeWindow(windowName, SCREEN_DIMS['width'], SCREEN_DIMS['height'])
+        # cv2.moveWindow(windowName, 0, -50)
+
+        #   add mouse listener function
+        cv2.setMouseCallback(windowName, closeEvent)
+
+        cropObjs = coordList()
+
+        #   add rectangles to the window
+        for obj in cropObjs.myList[1:]:
+            cv2.rectangle(image, obj.topL, obj.botR, (0, 255, 0), 3)
+
+        cv2.imshow(windowName, image)
+        cv2.waitKey(0)
+
+
+#   Use this event listener if you want to close after one click only
+def closeEvent(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONUP:
+        print("Closing...")
+        param.append(True)
