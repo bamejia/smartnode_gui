67902f23471f538803a52ae88adbde8e8ed808a7
lstubbolo <lstubbol@udel.edu>
2020-05-09 17:36:03 -0400

    noooo.....

--- ocr_gui.py
+++ ocr_gui.py
@@ -71,37 +71,37 @@ class OCRRuntime(tk.Frame):
             self.button_off = False
             return
 
-        #   this is the function that handles the individual steps for a single ocr sampling run
-        def ocr_run_once(self):
-            print("OCR_RUNTIME LOOP: " + str(self.count))
-            # mySet = settings.loadSettings('OCRSettings.json')
+    #   this is the function that handles the individual steps for a single ocr sampling run
+    def ocr_run_once(self):
+        print("OCR_RUNTIME LOOP: " + str(self.count))
+        # mySet = settings.loadSettings('OCRSettings.json')
 
-            #   load ocrSettings / ocr output file, OCRData.json
-            ocrData = settings.loadSettings('OCRData.json')
+        #   load ocrSettings / ocr output file, OCRData.json
+        ocrData = settings.loadSettings('OCRData.json')
 
-            #   capture / crop source image
-            image.takeSource()
-            image.cropSource(debug=True)
+        #   capture / crop source image
+        image.takeSource()
+        image.cropSource(debug=True)
 
-            #   perform ocr on all cropped images
-            ocrData = ocr.do_OCR_all(ocrData, debug=True)
+        #   perform ocr on all cropped images
+        ocrData = ocr.do_OCR_all(ocrData, debug=True)
 
-            #   temporary printout of data captured during this run
-            #   this information needs to be passed to display, firebase
-            #   dataset is a dict saved in ocrData.json
-            #       -> objects have same format as OCR_DATA_ENTRY in DEFAULTS
+        #   temporary printout of data captured during this run
+        #   this information needs to be passed to display, firebase
+        #   dataset is a dict saved in ocrData.json
+        #       -> objects have same format as OCR_DATA_ENTRY in DEFAULTS
 
-            print("\nOCR data Captured:")
-            dataSet = ocrData['dataset']
+        print("\nOCR data Captured:")
+        dataSet = ocrData['dataset']
 
-            #   note -> dataSet[entry] and dataSet[entry]['name'] are the same string...
-            for entry in dataSet:
-                print(f"\t{dataSet[entry]['name']}: '{dataSet[entry]['text']}'")
+        #   note -> dataSet[entry] and dataSet[entry]['name'] are the same string...
+        for entry in dataSet:
+            print(f"\t{dataSet[entry]['name']}: '{dataSet[entry]['text']}'")
 
-            #   loop control variables
-            self.count += 1
-            loop_again = False
-            return loop_again
+        #   loop control variables
+        self.count += 1
+        loop_again = False
+        return loop_again
 
     # Starts the loop to call OCR called by button
     def ocr_on_off(self):
