27f73065e568003e5a4060af2b034c031a765ec1
bamejia <bamejia@udel.edu>
2020-05-10 18:24:59 -0400

    added finger press button to finger settings

--- finger_gui.py
+++ finger_gui.py
@@ -12,10 +12,28 @@ class FingerSettings(tk.Frame):
         label = gbl.GLabel(self, text="Finger Settings", font=controller.title_font)
         label.pack(side="top", fill="x", pady=10)
 
+        self.finger_press = True
+
+        press_finger_func = lambda :(
+            self.finger_press()
+        )
+
         back_btn_func = lambda: (
-                                 controller.show_frame("Settings"))
+            controller.show_frame("Settings")
+        )
+
+        pressfinger_btn = gbl.GButton(self, text="Press Finger",
+                           command=press_finger_func)
 
         back_btn = gbl.GButton(self, text="Go back",
                            command=back_btn_func)
 
-        back_btn.pack()
+
+        pressfinger_btn.pack(pady=gv.BUTTON_SPACE)
+        back_btn.pack(pady=gv.BUTTON_SPACE)
+
+    def press_finger(self):
+        if self.finger_press:
+            self.finger_press = False
+            delay, repeats, interval = 500, 0, 500
+            finger.finger_looper(self.after, self.set_finger_press, delay, repeats, interval)
